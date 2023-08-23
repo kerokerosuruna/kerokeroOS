c28f4c30777ec8a389f2d469fc226e7c598e1410
Jacob <sharkboygippster@gmail.com>
2023-08-22 21:22:04 -0400

    a bit is done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+//cargo rustc -- -C link-args="/ENTRY:_start /SUBSYSTEM:console"
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! { //enter
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { //TODO: Make this do something
+    loop {}
 }
