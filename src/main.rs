241958e641c55bf1cf7b790d76c33f402257de87
Jacob <sharkboygippster@gmail.com>
2023-08-26 15:48:03 -0400

    i guess i can release it now

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+static TEXT: &[u8] = b"There is nothing to see here";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! { //enter
     let vga_buff = 0xb8000 as *mut u8;
-
-
-    for (i, &byte) in HELLO.iter().enumerate() {
+    let mut colour: u8 = 0x1;
+    
+    for (i, &byte) in TEXT.iter().enumerate() {
         unsafe {
             *vga_buff.offset(i as isize * 2) = byte;
-            *vga_buff.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buff.offset(i as isize * 2 + 1) = colour;
+            colour += 1;
+            if colour > 0xf{
+                colour = 0x1;
+            }
         }
     }
     loop {}
